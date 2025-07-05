09c23e4d69fa64e6d1e9a5fac26ba680ceae0365
Cynthia <parmarvaidehi437@gmail.com>
2025-07-05 19:33:46 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    return "Hello, " + name;
+}
+
+let ans = greet('vaidehi')
+let ans1 = greet('Jiya')
+
+console.log(ans)
+console.log(ans1)
---
+++ if-else.js
@@ -0,0 +1,12 @@
+function agee(age) {
+    if (age >=18) {
+        console.log('You are an ADULT.')
+    } else {
+        console.log('You are a MINOR.')
+    }
+}
+let user1 = agee(12)
+let user2 = agee(3)
+
+console.log(user1)
+console.log(user2)
