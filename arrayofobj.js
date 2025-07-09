fee5951e5b5dff29c37dc7e6e33c30d114002ec6
Cynthia <parmarvaidehi437@gmail.com>
2025-07-09 11:08:59 +0530

    Array of Objects

---
+++ arrayofobj.js
@@ -0,0 +1,14 @@
+let arr = ["Vaidehi", 17, {
+    name : "Vaidehi",
+    age : 17
+}];
+
+const val1 = arr[2];
+
+const val2 = {
+    name : "Vaidehi",
+    age : 17
+}
+
+console.log(val1);
+console.log(val2);
