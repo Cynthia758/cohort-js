298d8a8bcf153b9647f9c6946861724182a71bdd
Cynthia <parmarvaidehi437@gmail.com>
2025-07-09 10:20:43 +0530

    assignment3

---
+++ assignment3.js
@@ -0,0 +1,15 @@
+function greetuser(user) {
+    console.log("namaste! " + user.name + ". your age is " + user.age + ".");
+    if(user.age > 18) {
+        console.log("You can Vote.");
+    } else {
+        console.log("You can't Vote.");
+    }
+}
+
+let user = {
+    name : "Vaidehi",
+    age : 90
+};
+
+greetuser(user);
