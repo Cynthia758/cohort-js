d8d8f25d1508846a5e89a383bca59f903bb50342
Cynthia <parmarvaidehi437@gmail.com>
2025-07-12 13:04:02 +0530

    if-else modified

--- if-else.js
+++ if-else.js
@@ -1,12 +1,13 @@
 function agee(age) {
     if (age >=18) {
-        console.log('You are an ADULT.')
+        return 'You are an ADULT.'
     } else {
-        console.log('You are a MINOR.')
+        return 'You are a MINOR.'
     }
 }
 let user1 = agee(12)
 let user2 = agee(3)
 
-console.log(user1)
-console.log(user2)
+console.log(user1);
+console.log(user2);
+
