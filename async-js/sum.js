d81b1980e7a40a5180c76a587383e68f6e50583b
Cynthia <parmarvaidehi437@gmail.com>
2025-07-12 15:42:51 +0530

    Modified

--- async-js/sum.js
+++ async-js/sum.js
@@ -1,5 +1,9 @@
+// TypeScript
+// Parse it to a number
 function sum(a,b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 let ans = sum(3,3)
+let ans1 = sum("3",3)
 console.log(ans);
+console.log(ans1);
